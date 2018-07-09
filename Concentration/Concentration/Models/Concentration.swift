e565e396d4de065d853678beb2d2119e3d0548f4
Rong <rayanlei@gmail.com>
2018-07-09 07:17:35 -0700

    closure, end Lecture 4

--- Concentration/Concentration/Models/Concentration.swift
+++ Concentration/Concentration/Models/Concentration.swift
@@ -13,17 +13,7 @@ struct Concentration {
     
     private var indexOfOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
         }
         set {
             for index in cards.indices {
@@ -60,3 +50,10 @@ struct Concentration {
         //TODO: Shuffle the cards
     }
 }
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
+    }
+}
+
