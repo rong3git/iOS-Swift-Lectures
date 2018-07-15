c27701b8513f8292b608b05c67e3e7c8943a8834
Rong <rayanlei@gmail.com>
2018-07-15 02:28:19 -0700

    set up model for generating random PlayingCard as SuitRank format
    (end lecture 5)

---
+++ PlayingCard/PlayingCard/PlayingCardDeck.swift
@@ -0,0 +1,42 @@
+//
+//  PlayingCardDeck.swift
+//  PlayingCard
+//
+//  Created by rongc on 7/14/18.
+//  Copyright © 2018 RongchangLei. All rights reserved.
+//  Swift allow nest type
+
+import Foundation
+
+struct PlayingCardDeck {
+    private(set) var cards = [PlayingCard]()
+    
+    init() {
+        for suit in PlayingCard.Suit.all {
+            for rank in PlayingCard.Rank.all {
+                cards.append(PlayingCard(suit: suit, rank: rank))
+            }
+        }
+    }
+    
+    mutating func draw() -> PlayingCard? {
+        if cards.count > 0 {
+            return cards.remove(at: cards.count.arc4random) //return that removed element
+        } else {
+            return nil
+        }
+    }
+}
+
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
+}
