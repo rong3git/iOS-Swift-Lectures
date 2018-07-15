5a72f1a71b95e9273612a0e6e0c17d02a4623f45
Rong <rayanlei@gmail.com>
2018-07-15 16:23:20 -0700

    view content mode - redraw, testing drawing

---
+++ PlayingCard/PlayingCard/PlayingCardView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by rongc on 7/15/18.
+//  Copyright © 2018 RongchangLei. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
